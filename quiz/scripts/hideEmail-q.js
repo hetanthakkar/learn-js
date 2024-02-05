ae6b94f97c6f1e57d3e8430d0d9bbd9929ffb73b
hetanthakkar <hetanthakkar1@gmail.com>
2024-02-04 21:31:22 -0500

    Hide email

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -2,10 +2,25 @@
 replace the username part with * characters.
 */
 const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
+function isValid(email) {
+    return /^[a-zA-Z0-9_]+@[a-z]+.[a-z]/.test(email)
+}
 window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
     for (const email of emails) {
+        if (email.match(/^[a-z0-9_]+@(.+\.)*northeastern.edu$/)) {
+            console.log(email);
+            const listItem = document.createElement('li');
+            listItem.textContent = email.slice(0, email.indexOf('@')) + email.slice('@', email.replace('/./g', '*'));
+            list.appendChild(listItem);
+        }
+        if (isValid(email)) {
+            //split based on @
+            //split[0].replace()
+        }
         // complete the loop
     }
+
+
 }
